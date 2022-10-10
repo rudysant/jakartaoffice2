fae174fe9a3e5171bc7357a23f0eefd6a67860ac
jakartaoffice <rudysant@gmail.com>
2022-10-10 07:58:05 +0000

    Add bootstrap

--- nlajkt_cat_stat/admin.py
+++ nlajkt_cat_stat/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 
-from . models import Catalogues
+from . models import Catalogues, Acquisition
 
 class CataloguesA(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_per_page = 10
 
-    
+class AcquisitionA(admin.ModelAdmin):
+    list_display = ('id', 'vendor')
+    list_per_page = 10
+
+
 
 admin.site.register(Catalogues, CataloguesA)
-
+admin.site.register(Acquisition, AcquisitionA)
 
 
 # Register your models here.
